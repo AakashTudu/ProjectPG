14cb149927453ce74126d350192b33389a202441
AakashTudu <44318049+AakashTudu@users.noreply.github.com>
2022-08-19 10:37:08 +0530

    Create AddPropActivity.java

---
+++ app/src/main/java/com/example/hostel/AddPropActivity.java
@@ -0,0 +1,53 @@
+package com.example.hostel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class AddPropActivity extends AppCompatActivity {
+
+    Button button;
+
+    Spinner spinner;
+    String[] months = {"Select Property Type", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_prop);
+
+        spinner = findViewById(R.id.spinner);
+        button = findViewById(R.id.button);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddPropActivity.this, android.R.layout.simple_spinner_item, months);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String value = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TotalFloorsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
