7f0e9bf79c45083e5af34100319f84fb581a0312
Anurag Sagar <anurag.sagar@sensen.ai>
2022-08-28 09:18:33 +0530

    Add Tenant section pages

---
+++ app/src/main/java/com/example/hostel/UI/SelectSharingActivity.java
@@ -0,0 +1,16 @@
+package com.example.hostel.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hostel.R;
+
+public class SelectSharingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_sharing);
+    }
+}
